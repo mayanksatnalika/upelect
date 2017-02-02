ea15e6198a9593c4e45d0bf0170401a8cc9039c7
mayanksatnalika <satnalikamayank12@gmail.com>
2017-02-02 16:41:39 +0530

    app pass

--- sendEmail.py
+++ sendEmail.py
@@ -6,7 +6,7 @@ def msgme(msg):
     import smtplib
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login("satnalikamayank12@gmail.com", "Anjuaarushi.11")
+    server.login("satnalikamayank12@gmail.com", "wkurfgeluqmcixlf")
     server.sendmail("satnalikamayank12@gmail.com", "satnalikamayank12@gmail.com", msg)
     server.quit()
     print 'email sent'
