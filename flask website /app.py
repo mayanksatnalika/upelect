838696a4cf9ec214edd3e4b049556f25990ad245
mayanksatnalika <satnalikamayank12@gmail.com>
2017-03-06 01:03:28 +0530

    heroku file settings

--- flask website /app.py
+++ flask website /app.py
@@ -15,4 +15,6 @@ def welcome():
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 80))
+    app.run(host='0.0.0.0', port=port)
